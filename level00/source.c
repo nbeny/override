109e907f29c71d91d7b5ad1ab20a9daee0d3500a
nbeny <nbeny@student.42.fr>
2020-03-06 14:47:12 +0100

    level00

---
+++ level00/source.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int local_14 [4];
+  
+  puts("***********************************");
+  puts("* \t     -Level00 -\t\t  *");
+  puts("***********************************");
+  printf("Password:");
+  __isoc99_scanf(&DAT_08048636,local_14);
+  if (local_14[0] != 0x149c) {
+    puts("\nInvalid Password!");
+  }
+  else {
+    puts("\nAuthenticated!");
+    system("/bin/sh");
+  }
+  return (int)(local_14[0] != 0x149c);
+}
